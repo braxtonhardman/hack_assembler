c16699de053b65510d985dd53e4795876f16a4e4
Ethan Emerson <eemerson0022@pointloma.edu>
2025-03-22 12:10:18 -0700

    Setup the basic assembler file stuff.

---
+++ Assembler.java
@@ -0,0 +1,17 @@
+//Java Assemlber Program
+//By Ethan Emerson, Braxton Hardman, Christian Sapp
+
+public class Assembler
+{
+    //Parser Module
+    class Parser
+    {
+
+    }
+
+    //Code module
+    class Code
+    {
+
+    }
+}
