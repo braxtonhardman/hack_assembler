c37981a2d74df3653e6852214fc8bf9d1f24e1b4
Ethan Emerson <eemerson0022@pointloma.edu>
2025-03-22 12:20:24 -0700

    Adding some files

--- Assembler.java
+++ Assembler.java
@@ -1,4 +1,4 @@
-//Java Assemlber Program
+//Java Assembler Program
 //By Ethan Emerson, Braxton Hardman, Christian Sapp
 
 public class Assembler
@@ -6,12 +6,17 @@ public class Assembler
     //Parser Module
     class Parser
     {
-
+        
     }
 
     //Code module
     class Code
     {
+        
+    }
 
+    public static void main(String []args) //main method that runs the program
+    {
+        System.out.println("Hello World!");
     }
 }
