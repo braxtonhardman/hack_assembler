7832d79112e2f2bae8076a29ad657ffc8d7895f7
Braxton Hardman <braxtonhardman@gmail.com>
2025-03-22 12:34:56 -0700

    Added all the codes for comp jump and dest

--- Code.java
+++ Code.java
@@ -1,7 +1,86 @@
+
 //Code Module
 //Ethan Emerson, Braxton Hardman, Christian Sapp
+import java.util.HashMap;
 
-public class Code
-{
+public class Code {
 
+	private HashMap<String, String> compCodes;
+	private HashMap<String, String> jumpCodes;
+	private HashMap<String, String> destCodes;
+
+	public Code() {
+		this.putCompCodes();
+		this.putJumpCodes();
+		this.putDestCodes();
+	}
+
+	private void putCompCodes() {
+		this.compCodes = new HashMap<>();
+		this.compCodes.put("0", "0101010");
+		this.compCodes.put("1", "0111111");
+		this.compCodes.put("-1", "0111010");
+		this.compCodes.put("D", "0001100");
+		this.compCodes.put("A", "0110000");
+		this.compCodes.put("!D", "0001101");
+		this.compCodes.put("!A", "0110001");
+		this.compCodes.put("-D", "0001111");
+		this.compCodes.put("-A", "0110011");
+		this.compCodes.put("D+1", "0011111");
+		this.compCodes.put("A+1", "0110111");
+		this.compCodes.put("D-1", "0001110");
+		this.compCodes.put("A-1", "0110010");
+		this.compCodes.put("D+A", "0000010");
+		this.compCodes.put("D-A", "0010011");
+		this.compCodes.put("A-D", "0000111");
+		this.compCodes.put("D&A", "0000000");
+		this.compCodes.put("D|A", "0010101");
+		this.compCodes.put("M", "1110000");
+		this.compCodes.put("!M", "1110001");
+		this.compCodes.put("-M", "1110011");
+		this.compCodes.put("M+1", "1110111");
+		this.compCodes.put("M-1", "1110010");
+		this.compCodes.put("D+M", "1000010");
+		this.compCodes.put("D-M", "1010011");
+		this.compCodes.put("M-D", "1000111");
+		this.compCodes.put("D&M", "1000000");
+		this.compCodes.put("D|M", "1010101");
+	}
+
+	private void putJumpCodes() {
+		this.jumpCodes = new HashMap<String, String>();
+		this.jumpCodes.put("NULL", "000");
+		this.jumpCodes.put("JGT", "001");
+		this.jumpCodes.put("JEQ", "010");
+		this.jumpCodes.put("JGE", "011");
+		this.jumpCodes.put("JLT", "100");
+		this.jumpCodes.put("JNE", "101");
+		this.jumpCodes.put("JLE", "110");
+		this.jumpCodes.put("JMP", "111");
+	}
+
+	private void putDestCodes() {
+		this.destCodes = new HashMap<String, String>();
+		this.destCodes.put("NULL", "000");
+		this.destCodes.put("M", "001");
+		this.destCodes.put("D", "010");
+		this.destCodes.put("MD", "011");
+		this.destCodes.put("A", "100");
+		this.destCodes.put("AM", "101");
+		this.destCodes.put("AD", "110");
+		this.destCodes.put("AMD", "111");
+
+	}
+
+	public String comp(String digits) {
+		return this.compCodes.get(digits);
+	}
+
+	public String dest(String digits) {
+		return this.destCodes.get(digits);
+	}
+
+	public String jump(String digits) {
+		return this.jumpCodes.get(digits);
+	}
 }
