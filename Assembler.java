bb574f13bf280828762fc0a5723236fc5fc01b60
Ethan Emerson <eemerson0022@pointloma.edu>
2025-03-24 21:01:52 -0700

    Added some binary C-instruction representability

--- Assembler.java
+++ Assembler.java
@@ -37,9 +37,8 @@ public class Assembler
                 
             } else { 
                 lineNumber++;
-                return;
             }
-            lineNumber++; 
+            lineNumber++;
         }
     }
 
@@ -52,18 +51,204 @@ public class Assembler
         while(parser.hasMoreLines()){
             parser.advance();
             char instructionType = parser.getInstructionType();
-            if(instructionType == 'c') { 
-                // If the command is a jump command split on the ; else split on the = 
-                if(parser.getjump() != null) { 
-                    String dest = parser.getDest(); 
-                    String jump = parser.getjump(); 
-                    System.out.println(dest + " " + jump);
-                } else { 
-                    String dest = parser.getDest(); 
-                    String comp = parser.getComp(); 
-                    System.out.println(dest + " " + comp);
+            if(instructionType == 'c') { //We now build out the binary string representation of the c instruction
+                //print out the first three bits of the c_instruction
+                System.out.print("111");
+
+                //Now check and see what the comp value is
+                String compString = parser.getComp();
+                if(compString != null){
+                    
+                    //Now run through all the different domains of possible combinations for the comp section
+                    if(compString.equals("0")) //0
+                    {
+                        System.out.print("0101010");
+                    }
+                    else if(compString.equals("1")) //1
+                    {
+                        System.out.print("0111111");
+                    }
+                    else if(compString.equals("-1"))//-1
+                    {
+                        System.out.println("0111010");
+                    }
+                    else if(compString.equals("D")) //D
+                    {
+                        System.out.print("0001100");
+                    }
+                    else if(compString.equals("A")) //A
+                    {
+                        System.out.print("0110000");
+                    }
+                    else if(compString.equals("M")) //M
+                    {
+                        System.out.print("1110000");
+                    }
+                    else if(compString.equals("!D")) //!D
+                    {
+                        System.out.print("0001101");
+                    }
+                    else if(compString.equals("!A")) //!A
+                    {
+                        System.out.print("0110001");
+                    }
+                    else if(compString.equals("A")) //!M
+                    {
+                        System.out.print("1110001");
+                    }
+                    else if(compString.equals("-D")) //-D
+                    {
+                        System.out.print("0001111");
+                    }
+                    else if(compString.equals("-A")) //-A
+                    {
+                        System.out.print("0110011");
+                    }
+                    else if(compString.equals("-M")) //-M
+                    {
+                        System.out.print("1110011");
+                    }
+                    else if(compString.equals("D+1") || compString.equals("1+D")) //D+1
+                    {
+                        System.out.print("0011111");
+                    }
+                    else if(compString.equals("A+1") || compString.equals("1+A")) //A+1
+                    {
+                        System.out.print("0110111");
+                    }
+                    else if(compString.equals("M+1") || compString.equals("1+M")) //M+1
+                    {
+                        System.out.print("1110111");
+                    }
+                    else if(compString.equals("D-1")) //D-1
+                    {
+                        System.out.print("0001110");
+                    }
+                    else if(compString.equals("A-1")) //A-1
+                    {
+                        System.out.print("0110010");
+                    }
+                    else if(compString.equals("M-1")) //M-1
+                    {
+                        System.out.print("1110010");
+                    }
+                    else if(compString.equals("D+A") || compString.equals("A+D")) //D+A
+                    {
+                        System.out.print("0000010");
+                    }
+                    else if(compString.equals("D+M") || compString.equals("M+D")) //D+M
+                    {
+                        System.out.print("1000010");
+                    }
+                    else if(compString.equals("D-A")) //D-A
+                    {
+                        System.out.print("0010011");
+                    }
+                    else if(compString.equals("D-M")) //D-M
+                    {
+                        System.out.print("1010011");
+                    }
+                    else if(compString.equals("A-D")) //A-D
+                    {
+                        System.out.print("0000111");
+                    }
+                    else if(compString.equals("M-D")) //M-D
+                    {
+                        System.out.print("1000111");
+                    }
+                    else if(compString.equals("D&A") || compString.equals("A&D")) //D&A
+                    {
+                        System.out.print("0000000");
+                    }
+                    else if(compString.equals("D&M") ||  compString.equals("M&D")) //D&A
+                    {
+                        System.out.print("1000000");
+                    }
+                    else if(compString.equals("D|A") ||  compString.equals("A|D")) //D|A
+                    {
+                        System.out.print("0010101");
+                    }
+                    else if(compString.equals("D|M") ||  compString.equals("M|D")) //D|M
+                    {
+                        System.out.print("1010101");
+                    }
+                    else //we have a null error
+                    {
+                        System.out.print("Non-recongized null in the comp section of the c instruction");
+                    }
+
+                    //Now look at the different destination bits
+                    String destString = parser.getDest();
+                    if(destString != null){
+                        if(destString.equals("M")){
+                            System.out.print("001");
+                        }
+                        else if(destString.equals("D")){
+                            System.out.print("010");
+                        }
+                        else if(destString.equals("DM")){
+                            System.out.print("011");
+                        }
+                        else if(destString.equals("A")){
+                            System.out.print("100");
+                        }
+                        else if(destString.equals("AM")){
+                            System.out.print("101");
+                        }
+                        else if(destString.equals("AD")){
+                            System.out.print("110");
+                        }
+                        else if(destString.equals("ADM")){
+                            System.out.print("111");
+                        }
+                        else{
+                            System.out.print("We have an null issue in the destination part of a c instruction");
+                        }
+                    }
+                    else{ //null destination
+                        System.out.print("000");
+                    }
+
+                    //Now finally look at the different jump combinations
+                    String jumpString = parser.getjump();
+                    if(jumpString != null){
+
+                        //Now work through all the combinations of Jumping
+                        if(jumpString.equals("JGT")){
+                            System.out.print("001");
+                        }
+                        else if(jumpString.equals("JEQ")){
+                            System.out.print("010");
+                        }
+                        else if(jumpString.equals("JGE")){
+                            System.out.print("011");
+                        }
+                        else if(jumpString.equals("JLT")){
+                            System.out.print("100");
+                        }
+                        else if(jumpString.equals("JNE")){
+                            System.out.print("101");
+                        }
+                        else if(jumpString.equals("JLE")){
+                            System.out.print("110");
+                        }
+                        else if(jumpString.equals("JMP")){
+                            System.out.print("111");
+                        }
+                        else{
+                            System.out.print("We have a null issue in the jump destination of C-instruction");
+                        }
+                    }
+                    else{ //null jump
+                        System.out.print("000");
+                    }
+
+                    //newline print
+                    System.out.println();
+
                 }
-                
+                else //we don't have a comp and now print out null valye
+                    System.out.println("0000000000000");
             } else if(instructionType == 'a') { 
                 // Check if the symbol exists in the table 
                 // if it does not exist than it is a number
