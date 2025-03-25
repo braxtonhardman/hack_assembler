f7f4d413d66044e5466931cc3dbdbfc97793d22e
Ethan Emerson <eemerson0022@pointloma.edu>
2025-03-24 21:18:02 -0700

    Fixed a parser error

--- Parser.java
+++ Parser.java
@@ -30,29 +30,31 @@ public class Parser
     public void advance(){ //advance to the next instuction
         boolean instructionSet = false;
         while(!instructionSet && file.hasNext()){ //loop file until we have instruction
-            currentInstruction = file.next();
-
+            currentInstruction = file.nextLine();
             //Check for white space and comments
-            if(!currentInstruction.contains("//")){ //we a non-comment instruction
+            try{
+                if(!currentInstruction.contains("//")){ //we a non-comment instruction
 
-                instructionSet = true; //breaks the loop
-
-                //Check for what instructions we have
-                if(currentInstruction.charAt(0) == '@'){ //a
-                    currentSymbol = currentInstruction.substring(1);
-                    instructionType = 'a';
-                }
-                else if(currentInstruction.charAt(0) == '(') //label instruction
-                {
-                    currentSymbol = currentInstruction.substring(1, currentInstruction.length() - 1); //cut off the parenthesis
-                    instructionType = 'l';
-                }
-                else //we have a C-instruction
-                {
-                    instructionType = 'c';
-                    currentSymbol = null;
+                    instructionSet = true; //breaks the loop
+                    //Check for what instructions we have
+                    if(currentInstruction.charAt(0) == '@'){ //a
+                        currentSymbol = currentInstruction.substring(1);
+                        instructionType = 'a';
+                    }
+                    else if(currentInstruction.charAt(0) == '(') //label instruction
+                    {
+                        currentSymbol = currentInstruction.substring(1, currentInstruction.length() - 1); //cut off the parenthesis
+                        instructionType = 'l';
+                    }
+                    else //we have a C-instruction
+                    {
+                        instructionType = 'c';
+                        currentSymbol = null;
+                    }
                 }
             }
+            catch(IndexOutOfBoundsException ioobe){ //this is to catch an index out of bounds error to help pass over whitespace
+            }   
         }
     }
 
